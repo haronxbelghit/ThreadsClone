0570b6d2d25a8b6669e1e6f958fa9fc4c6a14bc1
Haron Belghit <haron.belghit@nestle.com>
2023-07-09 22:18:30 +0100

    Lots of UI changes and improvements, it's all coming together

--- ThreadsClone/ContentView.swift
+++ ThreadsClone/ContentView.swift
@@ -9,80 +9,116 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        ThreadsContent()
-            .background(Color(UIColor.systemBackground))
+        VStack(spacing: 0) {
+            ThreadsContent()
+                .background(Color(UIColor.systemBackground))
+            Spacer()
+        }
+        .ignoresSafeArea(.keyboard)
     }
 }
 
 struct ThreadsContent: View {
     let threads = GenerateThreadData.generate()
-    @State private var showSheet = false
-    @State private var columnHeightDp: CGFloat = 0
+    @State private var selectedTab: Tab = .home
+    
+    enum Tab {
+        case home
+        case search
+        case edit
+        case favorite
+        case profile
+    }
     
     var body: some View {
-        TabView {
-            LazyVStack {
-                HStack {
-                    Image("threadlogo")
-                        .resizable()
-                        .frame(width: 40, height: 40)
+        VStack(spacing: 0) {
+            ScrollView {
+                LazyVStack {
+                    HStack {
+                        Image("threadlogo")
+                            .resizable()
+                            .frame(width: 35, height: 35)
+                    }
+                    ForEach(threads) { thread in
+                        ThreadCard(thread: thread)
+                        Divider()
+                            .foregroundColor(Color.gray.opacity(0.1))
+                    }
                 }
-                ForEach(threads) { thread in
-                    ThreadCard(thread: thread)
-                    Divider()
-                        .foregroundColor(Color.gray.opacity(0.1))
-                }
-            }
-            .padding()
-            .onPreferenceChange(ColumnHeightPreferenceKey.self) { columnHeight in
-                columnHeightDp = columnHeight
-            }
-            .tabItem {
-                Image(systemName: "house")
-            }
+                .padding()
+            }.padding(.top, 1)
             
-            Text("Search")
-                .tabItem {
-                    Image(systemName: "magnifyingglass")
-                }
-            
-            Text("Edit")
-                .tabItem {
-                    Image(systemName: "pencil")
-                }
-            
-            Text("Favorite")
-                .tabItem {
-                    Image(systemName: "heart")
-                }
-            
-            Text("Profile")
-                .tabItem {
-                    Image(systemName: "person")
-                }
+            CustomTabBar(selectedTab: $selectedTab)
         }
         .background(Color(UIColor.systemBackground))
     }
 }
 
+struct CustomTabBar: View {
+    @Binding var selectedTab: ThreadsContent.Tab
+    
+    var body: some View {
+        HStack(spacing: -24) {
+            TabBarButton(tab: .home, imageName: "house", selectedTab: $selectedTab)
+            
+            Spacer()
+            
+            TabBarButton(tab: .search, imageName: "magnifyingglass", selectedTab: $selectedTab)
+            
+            Spacer()
+            
+            TabBarButton(tab: .edit, imageName: "pencil", selectedTab: $selectedTab)
+            
+            Spacer()
+            
+            TabBarButton(tab: .favorite, imageName: "heart", selectedTab: $selectedTab)
+            
+            Spacer()
+            
+            TabBarButton(tab: .profile, imageName: "person", selectedTab: $selectedTab)
+        }
+        .padding(.vertical, 8)
+        .background(Color.white)
+        .shadow(color: Color.black.opacity(0.01), radius: 2, x: 0, y: -2)
+        .edgesIgnoringSafeArea(.bottom)
+    }
+}
+
+struct TabBarButton: View {
+    let tab: ThreadsContent.Tab
+    let imageName: String
+    @Binding var selectedTab: ThreadsContent.Tab
+    
+    var body: some View {
+        Button(action: {
+            selectedTab = tab
+        }) {
+            VStack(spacing: 4) {
+                Image(systemName: imageName)
+                    .font(.system(size: 24))
+                    .foregroundColor(selectedTab == tab ? .black : .gray)
+            }
+        }
+        .frame(maxWidth: .infinity)
+    }
+}
+
 struct ThreadCard: View {
     @State private var showSheet = false
-    @State private var columnHeightPx: CGFloat = 0
+    @State private var isLiked = false
     
     let thread: Thread
     
     var body: some View {
-        HStack(alignment: .top, spacing: 8) {
+        HStack(alignment: .top, spacing: 12) {
             Image(thread.profileImage)
                 .resizable()
                 .aspectRatio(contentMode: .fill)
-                .frame(width: 50, height: 50)
+                .frame(width: 40, height: 40)
                 .clipShape(Circle())
-            Spacer()
-                .frame(width: 2, height: columnHeightPx - 70)
-                .background(Color.gray.opacity(0.4))
-                .alignmentGuide(.bottom) { d in d[VerticalAlignment.bottom] }
-            VStack(alignment: .leading, spacing: 8) {
+                .padding(.trailing, 8)
+            
+            VStack(alignment: .leading, spacing: 4) {
                 HStack(spacing: 2) {
                     Text(thread.name)
                         .font(.headline)
@@ -91,16 +127,20 @@ struct ThreadCard: View {
                             .resizable()
                             .frame(width: 16, height: 16)
                     }
-                }
-                HStack(spacing: 8) {
+                    Spacer()
                     Text(thread.time)
-                        .font(.system(size: 12))
-                        .fontWeight(.light)
+                        .font(.system(size: 14))
+                        .foregroundColor(.gray)
                     Button(action: { showSheet = true }) {
                         Image("more")
+                            .padding(.leading, 8)
                     }
                 }
+                .padding(.bottom, 2)
+                
                 Text(thread.message)
+                    .padding(.bottom, 4)
+                
                 if let messagePhoto = thread.messagePhoto {
                     Image(messagePhoto)
                         .resizable()
@@ -108,35 +148,136 @@ struct ThreadCard: View {
                         .frame(maxHeight: 200)
                         .cornerRadius(16)
                         .border(Color.gray.opacity(0.4), width: 2)
+                        .padding(.bottom, 4)
                 }
-                HStack(spacing: 0) {
-                    IconButton(imageName: "favorite", rotation: Angle.degrees(0))
-                    IconButton(imageName: "comment", rotation: .degrees(180))
+                
+                HStack(spacing: 16) {
+                    LikeButton(isLiked: $isLiked)
+                    IconButton(imageName: "comment", rotation: .degrees(0))
                     IconButton(imageName: "retweet", rotation: .degrees(180))
                     IconButton(imageName: "send", rotation: .degrees(-45))
                 }
+                .padding(.bottom, 8)
+                .padding(.top, 4)
+                
                 HStack(spacing: 8) {
                     Text("164 replies")
+                        .font(.system(size: 14))
                         .foregroundColor(.gray)
                     Text("99 likes")
+                        .font(.system(size: 14))
                         .foregroundColor(.gray)
                 }
             }
+            .padding(.trailing, 8)
         }
         .padding(.vertical, 8)
-        .onPreferenceChange(ColumnHeightPreferenceKey.self) { columnHeight in
-            columnHeightPx = columnHeight
-        }
         .sheet(isPresented: $showSheet) {
-            MoreBottomSheet()
+            MoreBottomSheet(showSheet: $showSheet)
         }
     }
 }
 
-struct MoreBottomSheet: View {
+struct LikeButton: View {
+    @Binding var isLiked: Bool
+    
     var body: some View {
-        // Your bottom sheet content
-        Text("More Bottom Sheet")
+        Button(action: { isLiked.toggle() }) {
+            ZStack {
+                Image(systemName: isLiked ? "heart.fill" : "heart")
+                    .resizable()
+                    .frame(width: 24, height: 22)
+                    .foregroundColor(isLiked ? .red : .black)
+            }
+        }
+        .font(Font.system(size: 20).weight(.semibold))
+    }
+}
+
+struct MoreBottomSheet: View {
+    @Environment(\.dismiss) private var dismiss
+    @Binding var showSheet: Bool
+
+    var body: some View {
+        VStack(spacing: 20) {
+            Spacer()
+
+            MoreBottomSheetContent(showSheet: $showSheet) // Pass the showSheet binding to the MoreBottomSheetContent
+                .background(Color(UIColor.systemBackground))
+                .cornerRadius(20)
+        }
+        .presentationDetents([.height(270)])
+        .presentationDragIndicator(.visible)
+        .frame(alignment: .bottom)
+        .edgesIgnoringSafeArea(.bottom)
+    }
+}
+
+struct MoreBottomSheetContent: View {
+    @Binding var showSheet: Bool // Add the showSheet binding
+
+    var body: some View {
+        VStack(spacing: 0) {
+            RoundedRectangle(cornerRadius: 20)
+                .fill(Color.gray.opacity(0.1))
+                .overlay(
+                    VStack(spacing: 0) {
+                        Button(action: { showSheet = false }) { // Use the showSheet binding to dismiss the sheet
+                            Text("Unfollow")
+                                .fontWeight(.semibold)
+                                .frame(maxWidth: .infinity, minHeight: 16)
+                                .padding()
+                                .background(Color.clear)
+                                .contentShape(Rectangle())
+                        }
+
+                        Divider().padding(0)
+
+                        Button(action: { showSheet = false }) { // Use the showSheet binding to dismiss the sheet
+                            Text("Mute")
+                                .fontWeight(.semibold)
+                                .frame(maxWidth: .infinity, minHeight: 16)
+                                .padding()
+                                .background(Color.clear)
+                                .contentShape(Rectangle())
+                        }
+                    }
+                )
+                .padding(.horizontal, 22)
+
+            Divider().padding(16)
+
+            VStack(spacing: 0) {
+                RoundedRectangle(cornerRadius: 12)
+                    .fill(Color.gray.opacity(0.1))
+                    .overlay(
+                        VStack(spacing: 0) {
+                            Button(action: { showSheet = false }) {
+                                Text("Hide")
+                                    .fontWeight(.semibold)
+                                    .padding()
+                                    .frame(maxWidth: .infinity, minHeight: 16)
+                                    .background(Color.clear)
+                                    .contentShape(Rectangle())
+                            }
+
+                            Divider()
+
+                            Button(action: {}) {
+                                Text("Report")
+                                    .fontWeight(.semibold)
+                                    .foregroundColor(.red)
+                                    .padding()
+                                    .frame(maxWidth: .infinity, minHeight: 16)
+                                    .background(Color.clear)
+                                    .contentShape(Rectangle())
+                            }
+                        }
+                    )
+                    .padding(.horizontal, 22)
+            }
+            Spacer(minLength: 16)
+        }
     }
 }
 
@@ -161,16 +302,6 @@ struct Thread: Identifiable {
     let message: String
     let profileImage: String
     let messagePhoto: String?
-}
-
-struct ColumnHeightPreferenceKey: PreferenceKey {
-    typealias Value = CGFloat
-    
-    static var defaultValue: CGFloat = 0
-    
-    static func reduce(value: inout CGFloat, nextValue: () -> CGFloat) {
-        value = max(value, nextValue())
-    }
 }
 
 struct GenerateThreadData {
