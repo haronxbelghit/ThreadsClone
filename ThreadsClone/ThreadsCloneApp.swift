44c837b5004c574e9a0b197af20706bd46edc85e
Haron Belghit <haron.belghit@nestle.com>
2023-07-09 15:00:28 +0100

    Initial Commit

---
+++ ThreadsClone/ThreadsCloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  ThreadsCloneApp.swift
+//  ThreadsClone
+//
+//  Created by Belghit Haron on 9/7/2023.
+//
+
+import SwiftUI
+
+@main
+struct ThreadsCloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
